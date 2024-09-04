2d88d0f1e67190e0f69b4d5988d44bffac3d1e36
MohamedYehia20m <77248801+MohamedYehia20m@users.noreply.github.com>
2024-09-04 10:05:29 +0200

    Update Lab1.java

--- Lab1.java
+++ Lab1.java
@@ -1,5 +1,73 @@
+package EBI;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class Lab1 {
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+    
+        // Lab1
+
+        int [] values = {3, 10, 6, 1, 4, 8, 2, 5, 9, 7};
+
+
+        System.out.printf("average: %.2f%n",
+                IntStream.of(values)
+                .average()
+                .getAsDouble()
+        );
+    
+
+        //Lab2
+    
+        List<String> stringList = new ArrayList<>();
+
+        stringList.add("Hello");
+        stringList.add("World");
+        stringList.add("Java");
+        stringList.add("Programming");
+
+        Stream<String> stream = stringList.stream();
+        stream.filter(e -> e != null)
+                .forEach(s ->System.out.printf("tolowercase strings: %s\n",s.toLowerCase()) );
+
+     
+
+        //Lab3
+    
+        List<String> stringList = new ArrayList<>();
+
+        stringList.add("Hello");
+        stringList.add("World");
+        stringList.add("Hello");
+        stringList.add("Programming");
+
+        Stream<String> stream = stringList.stream();
+        stream.distinct()
+                .forEach(s ->System.out.printf("distinct strings: %s\n",s) );
+     
+
+        //Lab4
+    
+        List<String> stringList = new ArrayList<>();
+
+        stringList.add("Hello");
+        stringList.add("World");
+        stringList.add("Hello");
+        stringList.add("Programming");
+
+        Stream<String> stream = stringList.stream();
+        long count = stream
+                .filter(s -> s.toLowerCase().startsWith("h"))
+                .count();
+
+        // Print the count
+        System.out.println("Number of strings starting with 's': " + count);
+
+     
     }
 }
